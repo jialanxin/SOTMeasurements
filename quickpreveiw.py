f657e75272a74e88b589335540fc587436941871
jialanxin <jialanxin1996@hotmail.com>
2022-03-24 00:17:59 +0800

    Delete quickpreveiw.py

--- quickpreveiw.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from preprocess import fitting,draw
-st.header('Quick Preview')
-data = st.file_uploader("Upload a STFMR data file", type="txt")
-if data is not None:
-    result = fitting(data)
-    fig = draw(result,"Empty")
-    st.write(fig)
-
